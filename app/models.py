d2be12e5f389474778a4c8466929ff4c3ccb415e
XIAC <x.i.a.c.1504@gmail.com>
2026-03-05 19:56:30 -0400

    agregacion de requerimientos y correccion de la contrasenia

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True ,nullable = False)
-    password = db.Column(db.String(100), nullable = False)
+    password = db.Column(db.String(250), nullable = False)
     role = db.Column(db.String(100), default="user")
     
     def set_password(self, password):
