933551898fe4a4d9c20303cb00642f089c3c4467
wined555 <wined555555@gmail.com>
2026-03-05 18:44:22 -0400

    Actividad 5 completada - EDWIN HUANCA ARO
    COMPLETADO

--- app/extensions.py
+++ app/extensions.py
@@ -1,8 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_admin import Admin
+from flask_migrate import Migrate  # <--- Agregamos esto
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 admin = Admin(name="Panel Administrador")
-login_manager.login_view = "login"
+migrate = Migrate()  # <--- Creamos la instancia de Migrate
+login_manager.login_view = "auth.login" # Ajustado para que coincida con tu Blueprint
